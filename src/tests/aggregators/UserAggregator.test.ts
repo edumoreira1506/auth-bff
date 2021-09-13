6bdbcdc92be6d99b7dcd0e837b51b2e8244c7574
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-13 19:28:28 -0300

    Fix tests

--- src/tests/aggregators/UserAggregator.test.ts
+++ src/tests/aggregators/UserAggregator.test.ts
@@ -1,7 +1,6 @@
 import faker from 'faker'
 
 import { UserAggregator } from '@Aggregators/UserAggregator'
-import { AuthError } from '@cig-platform/core'
 
 describe('UserAggregator', () => {
   describe('auth', () => {
@@ -10,19 +9,19 @@ describe('UserAggregator', () => {
       const mockAccountServiceClient: any = {
         authUser: jest.fn().mockResolvedValue(token)
       }
-      const userAggregator = new UserAggregator(mockAccountServiceClient)
+      const userAggregator = new UserAggregator(mockAccountServiceClient, {} as any)
 
       expect(await userAggregator.auth(faker.internet.email(), faker.internet.password())).toBe(token)
     })
 
-    it('throwns an error when the account service client does not return a valid token', async () => {
-      const invalidToken = null
+    it('throwns an error when the account service gets an error', async () => {
+      const error = new Error()
       const mockAccountServiceClient: any = {
-        authUser: jest.fn().mockResolvedValue(invalidToken)
+        authUser: jest.fn().mockRejectedValue(error)
       }
-      const userAggregator = new UserAggregator(mockAccountServiceClient)
+      const userAggregator = new UserAggregator(mockAccountServiceClient, {} as any)
 
-      await expect(userAggregator.auth).rejects.toThrow(AuthError)
+      await expect(userAggregator.auth).rejects.toThrow(error)
     })
   })
 })
