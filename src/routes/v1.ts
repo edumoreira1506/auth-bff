306364f933539a8f6f22060878038549a21c2da1
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-01 22:31:14 -0300

    Remove edit breeder route

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -3,9 +3,7 @@ import { withBodyValidation } from '@cig-platform/core'
 
 import UserController from '@Controllers/UserController'
 import { authUserSchema, storeUserSchema } from '@Schemas/UserSchemas'
-import BreederController from '@Controllers/BreederController'
 import withTokenAuthorization from '@Middlewares/withTokenAuthoritzation'
-import withBreederPermission from '@Middlewares/withBreederPermission'
 
 const router = express.Router()
 
@@ -14,7 +12,5 @@ router.post('/auth', withBodyValidation(authUserSchema), UserController.auth)
 router.post('/refresh-token', withTokenAuthorization, UserController.refreshToken)
 
 router.post('/users', withBodyValidation(storeUserSchema), UserController.store)
-
-router.patch('/breeders/:breederId', withTokenAuthorization, withBreederPermission, BreederController.update)
 
 export default router
