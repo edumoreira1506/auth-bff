9a31cc13d69ae15af7ed31fb12c960df4c6fd812
edumoreira1506 <contato@eduardoem.com.br>
2022-09-10 15:50:15 -0300

    Set api keys

--- src/clients/AccountServiceClient.ts
+++ src/clients/AccountServiceClient.ts
@@ -1,4 +1,5 @@
 import { AccountServiceClient } from '@cig-platform/core'
 import { ACCOUNT_SERVICE_URL } from '@Constants/account'
+import { ACCOUNT_SERVICE_API_KEY } from '@Constants/api-keys'
 
-export default new AccountServiceClient(ACCOUNT_SERVICE_URL)
+export default new AccountServiceClient(ACCOUNT_SERVICE_URL, ACCOUNT_SERVICE_API_KEY)
--- src/clients/AdvertisingServiceClient.ts
+++ src/clients/AdvertisingServiceClient.ts
@@ -1,4 +1,5 @@
 import { AdvertisingServiceClient } from '@cig-platform/core'
 import { ADVERTISING_SERVICE_URL } from '@Constants/advertising'
+import { ADVERTISING_SERVICE_API_KEY } from '@Constants/api-keys'
 
-export default new AdvertisingServiceClient(ADVERTISING_SERVICE_URL)
+export default new AdvertisingServiceClient(ADVERTISING_SERVICE_URL, ADVERTISING_SERVICE_API_KEY)
